39179016a9e941fb8e85e33f5d25b1d977462e59
Febby <gunawan.febby@gmail.com>
2021-07-22 14:48:43 +0700

    Update App.tsx
    - Move the set modal to separate function

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import ItemsModal from './components/ItemModal';
 
 function App() {
   const [showModal, setShowModal] = React.useState(false);
-
+  const displayModal = () => setShowModal(true);
   return (
     <>
       <div className="w-2/3 h-2/3 bg-gray-200 m-64 shadow-2xl rounded-lg mx-auto text-center py-12">
@@ -12,7 +12,11 @@ function App() {
           Product Info Placeholder
         </h2>
         <div className="mt-8 flex justify-center">
-          <Button data-testid="add-to-cart" color="indigo" onClick={() => setShowModal(true)}>
+          <Button
+            data-testid="add-to-cart"
+            color="indigo"
+            onClick={displayModal}
+          >
             ADD TO CART
           </Button>
         </div>
